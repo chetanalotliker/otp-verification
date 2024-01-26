2fe0c4a2df3a96e49eb4fbe11413be3decb4bd33
chetana.lotliker <chetanalotliker@gmail.com>
2024-01-27 00:00:47 +0530

    Create functionality for moving next input and button

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ inputs.addEventListener("keyup", function (e) {
         return;
     }
 });
+
+function verifyOtp() {
+    const input1 = document.getElementById('input1').value;
+    const input2 = document.getElementById('input2').value;
+    const input3 = document.getElementById('input3').value;
+    const input4 = document.getElementById('input4').value;
+
+    if (input1 && input2 && input3 && input4) {
+        document.getElementById('main_container').innerHTML = `<h4>OTP Verified Successfully </h4><a href=index.html data-toggle="tooltip" title="Go to Home Page"
+        ><i class="fa fa-home home-class"></i></a>`
+    }
+}
